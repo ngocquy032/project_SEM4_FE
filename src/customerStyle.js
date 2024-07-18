f7a53b441d7a4025dd9945f04a6aa9713a19ab20
ngocquy032 <ngocquy2412nd@gmail.com>
2024-07-18 21:27:07 +0700

    quy comit foder

---
+++ src/customerStyle.js
@@ -0,0 +1,3 @@
+import './assets/customers/css/style.css';
+import './assets/customers/js/script';
+import './assets/customers/js/contact';
--- src/views/Customers/pages/header.jsx
+++ src/views/Customers/pages/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../../../customerStyle'
 function Header(props) {
     return (
         <div>
