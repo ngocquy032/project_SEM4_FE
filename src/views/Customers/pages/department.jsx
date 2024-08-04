ee7dbd20fbd0a34a409ce390907d164d3613547b
TranTheSon2206 <109707502+TranTheSon2206@users.noreply.github.com>
2024-08-04 21:57:24 +0700

    /details-department

--- src/views/Customers/pages/department.jsx
+++ src/views/Customers/pages/department.jsx
@@ -1,108 +1,106 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Department = () => {
-    return (
-        <div>
-            <section className="page-title bg-1">
-                <div className="overlay"></div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="block text-center">
-                                <span className="text-white">All Department</span>
-                                <h1 className="text-capitalize mb-5 text-lg">Care Department</h1>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-
-            <section class="section service-2">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-7 text-center">
-                        <div class="section-title">
-                            <h2>Award winning patient care</h2>
-                            <div class="divider mx-auto my-4"></div>
-                            <p>Lets know moreel necessitatibus dolor asperiores illum possimus sint voluptates incidunt molestias nostrum laudantium. Maiores porro cumque quaerat.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-lg-4 col-md-6 ">
-                        <div class="department-block mb-5">
-                            <img src="customer/images/service/service-1.jpg" alt="" class="img-fluid w-100"/>
-                                <div class="content">
-                                    <h4 class="mt-4 mb-2 title-color">Opthomology</h4>
-                                    <p class="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
-                                    <a href="department-single.html" class="read-more">Learn More  <i class="icofont-simple-right ml-2"></i></a>
-                                </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6">
-                        <div class="department-block mb-5">
-                            <img src="customer/images/service/service-2.jpg" alt="" class="img-fluid w-100"/>
-                                <div class="content">
-                                    <h4 class="mt-4 mb-2  title-color">Cardiology</h4>
-                                    <p class="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
-                                    <a href="department-single.html" class="read-more">Learn More <i class="icofont-simple-right ml-2"></i></a>
-                                </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6">
-                        <div class="department-block mb-5">
-                            <img src="customer/images/service/service-3.jpg" alt="" class="img-fluid w-100"/>
-                                <div class="content">
-                                    <h4 class="mt-4 mb-2 title-color">Dental Care</h4>
-                                    <p class="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
-                                    <a href="department-single.html" class="read-more">Learn More <i class="icofont-simple-right ml-2"></i></a>
-                                </div>
-                        </div>
-                    </div>
-
-
-                    <div class="col-lg-4 col-md-6 ">
-                        <div class="department-block  mb-5 mb-lg-0">
-                            <img src="customer/images/service/service-4.jpg" alt="" class="img-fluid w-100"/>
-                                <div class="content">
-                                    <h4 class="mt-4 mb-2 title-color">Child Care</h4>
-                                    <p class="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
-                                    <a href="department-single.html" class="read-more">Learn More <i class="icofont-simple-right ml-2"></i></a>
-                                </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6">
-                        <div class="department-block mb-5 mb-lg-0">
-                            <img src="customer/images/service/service-6.jpg" alt="" class="img-fluid w-100"/>
-                                <div class="content">
-                                    <h4 class="mt-4 mb-2 title-color">Pulmology</h4>
-                                    <p class="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
-                                    <a href="department-single.html" class="read-more">Learn More <i class="icofont-simple-right ml-2"></i></a>
-                                </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6">
-                        <div class="department-block mb-5 mb-lg-0">
-                            <img src="customer/images/service/service-8.jpg" alt="" class="img-fluid w-100"/>
-                                <div class="content">
-                                    <h4 class="mt-4 mb-2 title-color">Gynecology</h4>
-                                    <p class="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
-                                    <a href="department-single.html" class="read-more">Learn More <i class="icofont-simple-right ml-2"></i></a>
-                                </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div>
+      <section className="page-title bg-1">
+        <div className="overlay"></div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="block text-center">
+                <span className="text-white">All Department</span>
+                <h1 className="text-capitalize mb-5 text-lg">Care Department</h1>
+              </div>
             </div>
-        </section>
+          </div>
         </div>
-    )
+      </section>
+
+      <section className="section service-2">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-7 text-center">
+              <div className="section-title">
+                <h2>Award winning patient care</h2>
+                <div className="divider mx-auto my-4"></div>
+                <p>Lets know moreel necessitatibus dolor asperiores illum possimus sint voluptates incidunt molestias nostrum laudantium. Maiores porro cumque quaerat.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-4 col-md-6">
+              <div className="department-block mb-5">
+                <img src="customer/images/service/service-1.jpg" alt="" className="img-fluid w-100" />
+                <div className="content">
+                  <h4 className="mt-4 mb-2 title-color">Opthomology</h4>
+                  <p className="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
+                  <Link to="/departmentDetails" className="read-more">Learn More  <i className="icofont-simple-right ml-2"></i></Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6">
+              <div className="department-block mb-5">
+                <img src="customer/images/service/service-2.jpg" alt="" className="img-fluid w-100" />
+                <div className="content">
+                  <h4 className="mt-4 mb-2 title-color">Cardiology</h4>
+                  <p className="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
+                  <Link to="/departmentDetails" className="read-more">Learn More <i className="icofont-simple-right ml-2"></i></Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6">
+              <div className="department-block mb-5">
+                <img src="customer/images/service/service-3.jpg" alt="" className="img-fluid w-100" />
+                <div className="content">
+                  <h4 className="mt-4 mb-2 title-color">Dental Care</h4>
+                  <p className="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
+                  <Link to="/departmentDetails" className="read-more">Learn More <i className="icofont-simple-right ml-2"></i></Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6">
+              <div className="department-block mb-5 mb-lg-0">
+                <img src="customer/images/service/service-4.jpg" alt="" className="img-fluid w-100" />
+                <div className="content">
+                  <h4 className="mt-4 mb-2 title-color">Child Care</h4>
+                  <p className="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
+                  <Link to="/departmentDetails" className="read-more">Learn More <i className="icofont-simple-right ml-2"></i></Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6">
+              <div className="department-block mb-5 mb-lg-0">
+                <img src="customer/images/service/service-6.jpg" alt="" className="img-fluid w-100" />
+                <div className="content">
+                  <h4 className="mt-4 mb-2 title-color">Pulmology</h4>
+                  <p className="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
+                  <Link to="/departmentDetails" className="read-more">Learn More <i className="icofont-simple-right ml-2"></i></Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6">
+              <div className="department-block mb-5 mb-lg-0">
+                <img src="customer/images/service/service-8.jpg" alt="" className="img-fluid w-100" />
+                <div className="content">
+                  <h4 className="mt-4 mb-2 title-color">Gynecology</h4>
+                  <p className="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
+                  <Link to="/departmentDetails" className="read-more">Learn More <i className="icofont-simple-right ml-2"></i></Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
 }
 
-export default Department
+export default Department;
--- src/views/Customers/pages/departmentDetails.jsx
+++ src/views/Customers/pages/departmentDetails.jsx
@@ -1,94 +1,156 @@
-import React from 'react'
-
-
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const DepartmentDetails = () => {
+    // Static data for the department
+    const department = {
+        name: 'Medicine and Health',
+        description: 'Detailed information about the department, including services provided and other relevant details.',
+        services: [
+            'International Drug Database',
+            'Stretchers and Stretcher Accessories',
+            'Cushions and Mattresses',
+            'Cholesterol and Lipid Tests',
+            'Critical Care Medicine Specialists',
+            'Emergency Assistance'
+        ],
+        price: '$120 per consultation',
+        address: '123 Health Street, Medicine City'
+    };
+
+    // Static data for the doctors
+    const doctors = [
+        {
+            name: 'Dr. John Doe',
+            specialty: 'Cardiologist',
+            experience: '15 years',
+            schedule: [
+                { day: 'Monday', time: '10:00 - 12:00' },
+                { day: 'Wednesday', time: '14:00 - 16:00' },
+                { day: 'Friday', time: '09:00 - 11:00' }
+            ]
+        },
+        {
+            name: 'Dr. Jane Smith',
+            specialty: 'Dermatologist',
+            experience: '10 years',
+            schedule: [
+                { day: 'Tuesday', time: '11:00 - 13:00' },
+                { day: 'Thursday', time: '15:00 - 17:00' },
+                { day: 'Saturday', time: '10:00 - 12:00' }
+            ]
+        }
+    ];
+
     return (
         <div>
-
-            <section class="page-title bg-1">
-            <div class="overlay"></div>
-            <div class="container">
-                <div class="row">
-                <div class="col-md-12">
-                    <div class="block text-center">
-                    <span class="text-white">Department Details</span>
-                    <h1 class="text-capitalize mb-5 text-lg">Single Department</h1>
-
+            <section className="page-title bg-1">
+                <div className="overlay"></div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="block text-center">
+                                <span className="text-white">Department Details</span>
+                                <h1 className="text-capitalize mb-5 text-lg">Single Department</h1>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                </div>
-            </div>
             </section>
 
-            <section class="section department-single">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="department-img">
-                                <img src="customers/images/service/bg-1.jpg" alt="" class="img-fluid"/>
+            <section className="section department-single">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="department-img">
+                                <img src="customers/images/service/bg-1.jpg" alt="" className="img-fluid" />
                             </div>
                         </div>
                     </div>
 
-                    <div class="row">
-                        <div class="col-lg-8">
-                            <div class="department-content mt-5">
-                                <h3 class="text-md">Medecine and Health</h3>
-                                <div class="divider my-4"></div>
-                                <p class="lead">Age forming covered you entered the examine. Blessing scarcely confined her contempt wondered shy. Dashwoods contented sportsmen at up no convinced cordially affection.</p>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum recusandae dolor autem laudantium, quaerat vel dignissimos. Magnam sint suscipit omnis eaque unde eos aliquam distinctio, quisquam iste, itaque possimus . Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet alias modi eaque, ratione recusandae cupiditate dolorum repellendus iure eius rerum hic minus ipsa at, corporis nesciunt tempore vero voluptas. Tempore.</p>
+                    <div className="row">
+                        <div className="col-lg-8">
+                            <div className="department-content mt-5">
+                                <h3 className="text-md">{department.name}</h3>
+                                <div className="divider my-4"></div>
+                                <p className="lead">{department.description}</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum recusandae dolor autem laudantium, quaerat vel dignissimos. Magnam sint suscipit omnis eaque unde eos aliquam distinctio, quisquam iste, itaque possimus.</p>
 
-
-                                <h3 class="mt-5 mb-4">Services features</h3>
-                                <div class="divider my-4"></div>
-                                <ul class="list-unstyled department-service">
-                                    <li><i class="icofont-check mr-2"></i>International Drug Database</li>
-                                    <li><i class="icofont-check mr-2"></i>Stretchers and Stretcher Accessories</li>
-                                    <li><i class="icofont-check mr-2"></i>Cushions and Mattresses</li>
-                                    <li><i class="icofont-check mr-2"></i>Cholesterol and lipid tests</li>
-                                    <li><i class="icofont-check mr-2"></i>Critical Care Medicine Specialists</li>
-                                    <li><i class="icofont-check mr-2"></i>Emergency Assistance</li>
+                                <h3 className="mt-5 mb-4">Services Features</h3>
+                                <div className="divider my-4"></div>
+                                <ul className="list-unstyled department-service">
+                                    {department.services.map((service, index) => (
+                                        <li key={index}><i className="icofont-check mr-2"></i>{service}</li>
+                                    ))}
                                 </ul>
-
-                                <a href="appoinment.html" class="btn btn-main-2 btn-round-full">Make an Appoinment<i class="icofont-simple-right ml-2  "></i></a>
                             </div>
                         </div>
 
-                        <div class="col-lg-4">
-                            <div class="sidebar-widget schedule-widget mt-5">
-                                <h5 class="mb-4">Time Schedule</h5>
-
-                                <ul class="list-unstyled">
-                                <li class="d-flex justify-content-between align-items-center">
-                                    <a href="#">Monday - Friday</a>
-                                    <span>9:00 - 17:00</span>
-                                </li>
-                                <li class="d-flex justify-content-between align-items-center">
-                                    <a href="#">Saturday</a>
-                                    <span>9:00 - 16:00</span>
-                                </li>
-                                <li class="d-flex justify-content-between align-items-center">
-                                    <a href="#">Sunday</a>
-                                    <span>Closed</span>
-                                </li>
+                        <div className="col-lg-4">
+                            <div className="sidebar-widget schedule-widget mt-5">
+                                <h5 className="mb-4">Time Schedule</h5>
+                                <ul className="list-unstyled">
+                                    <li className="d-flex justify-content-between align-items-center">
+                                        <a href="#">Monday - Friday</a>
+                                        <span>9:00 - 17:00</span>
+                                    </li>
+                                    <li className="d-flex justify-content-between align-items-center">
+                                        <a href="#">Saturday</a>
+                                        <span>9:00 - 16:00</span>
+                                    </li>
+                                    <li className="d-flex justify-content-between align-items-center">
+                                        <a href="#">Sunday</a>
+                                        <span>Closed</span>
+                                    </li>
                                 </ul>
 
-                                <div class="sidebar-contatct-info mt-4">
-                                    <p class="mb-0">Need Urgent Help?</p>
-                                    <h3>+23-4565-65768</h3>
+                                <div className="department-price mt-4">
+                                    <h5>Price</h5>
+                                    <p>{department.price}</p>
+                                </div>
+                                <div className="department-address mt-4">
+                                    <h5>Address</h5>
+                                    <p>{department.address}</p>
                                 </div>
                             </div>
                         </div>
                     </div>
+
+                    <div className="row mt-5">
+                        <div className="col-lg-12">
+                            <h3 className="mb-4">Doctors in this Department</h3>
+                            <div className="divider my-4"></div>
+                            <div className="row">
+                                {doctors.map((doctor, index) => (
+                                    <div key={index} className="col-md-6 mb-4">
+                                        <div className="card">
+                                            <div className="card-body">
+                                                <h4 className="card-title">{doctor.name}</h4>
+                                                <p className="card-text">Specialty: {doctor.specialty}</p>
+                                                <p className="card-text">Experience: {doctor.experience}</p>
+                                                <h5 className="card-subtitle mb-2">Schedule:</h5>
+                                                <ul>
+                                                    {doctor.schedule.map((slot, i) => (
+                                                        <li key={i}>{slot.day}: {slot.time}</li>
+                                                    ))}
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row mt-5">
+                    <div className="col-lg-12 text-center">
+                            <Link to="/bookAppoinment" className="btn btn-main-2 btn-round-full">Make an Appointment<i className="icofont-simple-right ml-2"></i></Link>
+                        </div>
+                    </div>
                 </div>
             </section>
-
-
-            
-
         </div>
-    )
+    );
 }
+
 export default DepartmentDetails;
